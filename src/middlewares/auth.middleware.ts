21225a3501c711b6b985220690c47267b449a617
Kartik Pal <kartik.p1110@gmail.com>
2024-11-03 20:17:58 +0530

    feat: auth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,16 +1,20 @@
-// import { Request, Response, NextFunction } from 'express';
-// import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
 
-// export const authenticateJWT = (req: Request, res: Response, next: NextFunction) => {
-//   const token = req.header('Authorization')?.split(' ')[1];
-//   if (!token) {
-//     return res.status(401).json({ error: 'Access denied, no token provided' });
-//   }
-//   try {
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET!);
-//     req.user = decoded;
-//     next();
-//   } catch (ex) {
-//     res.status(400).json({ error: 'Invalid token' });
-//   }
-// };
+export const authenticateJWT = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.header("Authorization")?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ error: "Access denied, no token provided" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+    (req as any).user = decoded;
+    next();
+  } catch (e) {
+    res.status(400).json({ error: "Invalid token" });
+  }
+};
