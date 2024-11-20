332c1656e232d1e3235f8b410cd733391b86963d
Kartik Pal <kartik.p1110@gmail.com>
2024-11-20 21:33:41 +0530

    fix: fixed enquiry route

--- src/routes/inquiries.route.ts
+++ src/routes/inquiries.route.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createInquiry, getInquiry } from './controller';
+import { createInquiry, getInquiry } from '../controllers/inquries.controller';
 
 const router = express.Router();
 
