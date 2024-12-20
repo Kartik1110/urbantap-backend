a480564ab641901ee6d44e845b636c512ae12688
Kartik Pal <kartik.p1110@gmail.com>
2024-12-21 01:48:23 +0530

    feat: modified create company api to return ids

--- src/services/company.service.ts
+++ src/services/company.service.ts
@@ -3,9 +3,23 @@ import { Company } from "@prisma/client";
 
 export const bulkInsertCompaniesService = async (companies: Company[]) => {
   try {
-    return await prisma.company.createMany({
+    await prisma.company.createMany({
       data: companies,
     });
+    
+    // Get the IDs of the newly created companies
+    const createdCompanies = await prisma.company.findMany({
+      where: {
+        name: {
+          in: companies.map(company => company.name)
+        }
+      },
+      select: {
+        id: true
+      }
+    });
+    
+    return createdCompanies.map(company => company.id);
   } catch (error) {
     throw error;
   }
