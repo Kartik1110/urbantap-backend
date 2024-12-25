4a4d502b71921001fa400b257a73ea8bc2c7e304
Kartik Pal <kartik.p1110@gmail.com>
2024-12-25 12:24:28 +0530

    feat: added SM role and Inquiries in notification

---
+++ prisma/migrations/20241225065357_sm_and_inquiries/migration.sql
@@ -0,0 +1,17 @@
+/*
+  Warnings:
+
+  - The values [Enquiries] on the enum `NotificationType` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "NotificationType_new" AS ENUM ('General', 'Inquiries', 'Network');
+ALTER TABLE "Notification" ALTER COLUMN "type" TYPE "NotificationType_new" USING ("type"::text::"NotificationType_new");
+ALTER TYPE "NotificationType" RENAME TO "NotificationType_old";
+ALTER TYPE "NotificationType_new" RENAME TO "NotificationType";
+DROP TYPE "NotificationType_old";
+COMMIT;
+
+-- AlterEnum
+ALTER TYPE "Role" ADD VALUE 'SM';
