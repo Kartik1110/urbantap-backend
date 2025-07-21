f4bca4ec4a66f9f79136554b608e528fa35891aa
Chinmay <chinmaybadwaik9@gmail.com>
2025-07-21 17:47:11 +0530

    Feat: jobs schema updated

--- src/routes/job.route.ts
+++ src/routes/job.route.ts
@@ -2,13 +2,13 @@ import multer from 'multer';
 import { Router } from 'express';
 
 import validateSchema from '../middlewares/validate.middleware';
-// import { jobSchema } from '../schema/job.schema';
+import { jobSchema } from '../schema/job.schema';
 import { applyJob, createJob, getJobs, getJobById } from '../controllers/job.controller';
 
 const router = Router();
 
 router.get('/jobs', getJobs);
-router.post('/job', createJob);
+router.post('/job', validateSchema(jobSchema), createJob);
 router.get('/jobs/:id', getJobById);
 
 export default (upload: multer.Multer) => {
