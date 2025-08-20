6bb84ed7bd4abe7754830c85e3d6b535a422d052
Chinmay <chinmaybadwaik9@gmail.com>
2025-08-20 20:06:34 +0530

    Revert "fix: profile_pic converted to file"
    This reverts commit 98f5fe83bff45cc629079344e554e99ab68b6b87.

--- src/routes/brokers.route.ts
+++ src/routes/brokers.route.ts
@@ -20,11 +20,11 @@ router.post('/brokers/:id/block', blockBroker);
 
 export default (upload: any) => {
     router.put('/brokers/:id', upload.fields([
-        { name: 'file', maxCount: 1 },
+        { name: 'profile_pic', maxCount: 1 },
         { name: 'cover_image', maxCount: 1 }
     ]), updateBroker);
     router.post('/brokers/bulk', upload.fields([
-        { name: 'file', maxCount: 1 },
+        { name: 'profile_pic', maxCount: 1 },
         { name: 'cover_image', maxCount: 1 }
     ]), bulkInsertBrokers);
     return router;
